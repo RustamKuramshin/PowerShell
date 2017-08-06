a1dbc125207f9c180b8a89a1e8ccef3ba3e43b28
Rustam Kuramshin <leo.in.empty@gmail.com>
2017-08-06 16:51:44 +0300

    Add files via upload

---
+++ ClearArch.ps1
@@ -0,0 +1,18 @@
+﻿$src = Get-ChildItem -recurse  D:\FTP -Include *.7z | Sort-Object -Descending CreationTime
+$origpath = @()
+foreach($a in $src){
+    try {
+        $arhpath = $a.FullName
+        & "C:\Program Files\7-Zip\7z.exe" t $arhpath
+        if($LASTEXITCODE){
+            Remove-Item $arhpath
+        }
+        else{
+            if($origpath -contains $a.DirectoryName){Remove-Item $arhpath}else{$origpath += $a.DirectoryName}
+        }
+        }
+    catch {
+        Write-Host "Не удалось выполнить команды"
+    }   
+}
+$origpath = 0
